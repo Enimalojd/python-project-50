4f1d47e7f582f1380efa05020392b0ec7ee35b5b
Enimalojd <enderston8@gmail.com>
2023-12-11 07:37:55 +0400

    add script for poetry

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,9 +1,10 @@
 from gendiff.cli import get_reference
+from gendiff.differ import generate_diff
 
 
 def main():
     args = get_reference()
-    print(args)
+    print(generate_diff(args.first_file, args.second_file))
 
 
 if __name__ == "__main__":
