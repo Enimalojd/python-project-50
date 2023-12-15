17a939f2f44ada03352be635f43d52bd82750268
Enimalojd <enderston8@gmail.com>
2023-12-15 20:19:49 +0400

    simplified func format_diff

--- gendiff/differ.py
+++ gendiff/differ.py
@@ -14,21 +14,24 @@ def compare_data(data1, data2):
     return diff
 
 
+def format_line(key, value1, value2):
+    if isinstance(value1, bool):
+        value1 = str(value1).lower()
+    if isinstance(value2, bool):
+        value2 = str(value2).lower()
+    if value1 is None:
+        return f"+ {key}: {value2}"
+    elif value2 is None:
+        return f"- {key}: {value1}"
+    elif value1 == value2:
+        return f"  {key}: {value1}"
+    else:
+        return f"- {key}: {value1}\n+ {key}: {value2}"
+
+
 def format_diff(diff):
-    lines = []
-    for key, (value1, value2) in sorted(diff.items()):
-        if isinstance(value1, bool):
-            value1 = str(value1).lower()
-        if isinstance(value2, bool):
-            value2 = str(value2).lower()
-        if value1 is None:
-            lines.append(f"+ {key}: {value2}")
-        elif value2 is None:
-            lines.append(f"- {key}: {value1}")
-        elif value1 == value2:
-            lines.append(f"  {key}: {value1}")
-        else:
-            lines.append(f"- {key}: {value1}\n+ {key}: {value2}")
+    lines = [format_line(key, value1, value2) for key,
+             (value1, value2) in sorted(diff.items())]
     return "\n".join(lines)
 
 
