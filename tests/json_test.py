db2b1d2ce5a468498b1101f76eea710e3ae9e8b7
Enimalojd <enderston8@gmail.com>
2023-12-19 13:15:02 +0400

    rename test

--- tests/json_test.py
+++ tests/json_test.py
@@ -6,7 +6,7 @@ import gendiff.differ as df
 def test_compare_json_files():
     path1 = 'tests/fixtures/file1.json'
     path2 = 'tests/fixtures/file2.json'
-    result_path = 'tests/fixtures/result.txt'
+    result_path = 'tests/fixtures/result_json.txt'
     
     with open(result_path, 'r') as f:
         result = f.read().strip()
