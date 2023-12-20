bde0f838e3186fb002ffb34e6860a557b9455a21
Enimalojd <enderston8@gmail.com>
2023-12-20 14:45:23 +0400

    test for nested json

---
+++ tests/json_nested_test.py
@@ -0,0 +1,14 @@
+import json
+import pytest
+import gendiff.differ as df
+
+
+def test_compare_json_files():
+    path1 = 'tests/fixtures/file1_nested.json'
+    path2 = 'tests/fixtures/file2_nested.json'
+    result_path = 'tests/fixtures/result_nested_json.txt'
+    
+    with open(result_path, 'r') as f:
+        result = f.read().strip()
+       
+    assert df.generate_diff(path1, path2) == result
