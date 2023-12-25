a684d477244d52f4b1a2c6300650d5550c64f9f2
Enimalojd <enderston8@gmail.com>
2023-12-25 10:34:30 +0400

    change logic for build diff

--- gendiff/differ.py
+++ gendiff/differ.py
@@ -1,37 +1,37 @@
+from .formatters.stylish import format_stylish
 from .parser import read_file
 
 
-def compare_data(data1, data2):
+
+def build_diff(data1, data2):
+    '''Построение дерева отличий'''
     diff = {}
-    for key in data1.keys() | data2.keys():
-        diff[key] = (data1.get(key), data2.get(key))
+    keys = sorted(data1.keys() | data2.keys())
+    for key in keys:
+        status = {'key': key}
+        if key not in data2:
+            status['operation'] = 'removed'
+            status['value'] = data1[key]
+        elif key not in data1:
+            status['operation'] = 'added'
+            status['value'] = data2[key]
+        elif data1[key] == data2[key]:
+            status['operation'] = 'unchanged'
+            status['value'] = data2[key]
+        elif isinstance(data1[key], dict) and isinstance(data2[key], dict):
+            status['operation'] = 'nested'
+            status['value'] = build_diff(data1[key], data2[key])
+        else:
+            status['operation'] = 'changed'
+            status['old'] = data1[key]
+            status['new'] = data2[key]
+        diff[key] = status
     return diff
 
 
-def format_line(key, value1, value2):
-    if isinstance(value1, bool):
-        value1 = str(value1).lower()
-    if isinstance(value2, bool):
-        value2 = str(value2).lower()
-    if value1 is None:
-        return f" + {key}: {value2}"
-    elif value2 is None:
-        return f" - {key}: {value1}"
-    elif value1 == value2:
-        return f"   {key}: {value1}"
-    else:
-        return f" - {key}: {value1}\n + {key}: {value2}"
-
-
-def format_diff(diff):
-    lines = [format_line(key, value1, value2) for key,
-             (value1, value2) in sorted(diff.items())]
-    result = "\n".join(lines)
-    return "{\n" + result + "\n}"
-
-
-def generate_diff(file_path1, file_path2):
+def generate_diff(file_path1, file_path2, format='stylish'):
     data1 = read_file(file_path1)
     data2 = read_file(file_path2)
-    diff = compare_data(data1, data2)
-    return format_diff(diff)
+    diff = build_diff(data1, data2)
+    if format == 'stylish':
+        return format_stylish(diff)
