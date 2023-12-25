33c4b392626eed3ddebbe3bccd649eb0cf28c0bc
Enimalojd <enderston8@gmail.com>
2023-12-25 15:33:36 +0400

    fix format

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,7 +4,7 @@ from gendiff.differ import generate_diff
 
 def main():
     args = get_reference()
-    print(generate_diff(args.first_file, args.second_file, args.format_name))
+    print(generate_diff(args.first_file, args.second_file, args.format))
 
 
 if __name__ == "__main__":
