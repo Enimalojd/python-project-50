2981e0c89a506c2b1a0d4fb3418848c7274c4c21
Enimalojd <enderston8@gmail.com>
2023-12-25 13:09:07 +0400

    fix

--- gendiff/differ.py
+++ gendiff/differ.py
@@ -1,4 +1,4 @@
-from .formatters.stylish import format_stylish
+from .formatter import format_selection
 from .parser import read_file
 
 
@@ -27,9 +27,9 @@ def build_diff(data1, data2):
     return diff
 
 
-def generate_diff(file_path1, file_path2, format='stylish'):
+def generate_diff(file_path1, file_path2, format_name='stylish'):
     data1 = read_file(file_path1)
     data2 = read_file(file_path2)
     diff = build_diff(data1, data2)
-    if format == 'stylish':
-        return format_stylish(diff)
+    return format_selection(diff, format_name)
+
