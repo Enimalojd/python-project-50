488b964521c09b4646ad28c30017bbb28b3f7a37
Enimalojd <enderston8@gmail.com>
2024-01-03 12:57:17 +0400

    split to 2 func

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -4,13 +4,15 @@ import yaml
 
 
 def read_file(file_path):
-    '''Определние формата файла и обработка.'''
     root, ext = os.path.splitext(file_path)
     with open(file_path, 'r', encoding="utf-8") as f:
-        if ext in ['.json']:
-            data = json.load(f)
-        elif ext in ['.yml', '.yaml']:
-            data = yaml.safe_load(f)
-        else:
-            raise ValueError(f'Unsupported file extension: {ext}')
-    return data
+        return parse_file(f.read(), ext)
+
+
+def parse_file(data, format):
+    if format in ['.json']:
+        return json.loads(data)
+    elif format in ['.yml', '.yaml']:
+        return yaml.safe_load(data)
+    else:
+        raise ValueError(f'Unsupported file extension: {format}')
