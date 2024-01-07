0bcd9ef84e9d8317401a40b99bb44889708bf240
Enimalojd <enderston8@gmail.com>
2024-01-07 17:54:32 +0400

    change tests to parametrize

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -10,34 +10,17 @@ def test_dir():
     return os.path.dirname(os.path.abspath(__file__))
 
 
-def test_compare_json_nested_files(test_dir):
-    path1 = os.path.join(test_dir, 'fixtures/file1_nested.json')
-    path2 = os.path.join(test_dir, 'fixtures/file2_nested.json')
-    result_path = os.path.join(test_dir, 'fixtures/result_nested_json.txt')
+@pytest.mark.parametrize(('file_path1', 'file_path2', 'result_path', 'format_type'), [
+    ('fixtures/file1_nested.json', 'fixtures/file2_nested.json', 'fixtures/result_nested_json.txt', 'stylish'),
+    ('fixtures/file1_nested.yml', 'fixtures/file2_nested.yml', 'fixtures/result_nested_yml.txt', 'stylish'),
+    ('fixtures/file1_nested.json', 'fixtures/file2_nested.json', 'fixtures/result_plain.txt', 'plain')
+])
+def test_compare_data(test_dir, file_path1, file_path2, result_path, format_type):
+    path1 = os.path.join(test_dir, file_path1)
+    path2 = os.path.join(test_dir, file_path2)
+    res_path = os.path.join(test_dir, result_path)
 
-    with open(result_path, 'r', encoding="utf-8") as f:
+    with open(res_path, 'r', encoding='utf-8') as f:
         result = f.read().strip()
 
-    assert df.generate_diff(path1, path2) == result
-
-
-def test_compare_yml_nested_files(test_dir):
-    path1 = os.path.join(test_dir, 'fixtures/file1_nested.yml')
-    path2 = os.path.join(test_dir, 'fixtures/file2_nested.yml')
-    result_path = os.path.join(test_dir, 'fixtures/result_nested_yml.txt')
-
-    with open(result_path, 'r', encoding="utf-8") as f:
-        result = f.read().strip()
-
-    assert df.generate_diff(path1, path2) == result
-
-
-def test_plain_format(test_dir):
-    path1 = os.path.join(test_dir, 'fixtures/file1_nested.json')
-    path2 = os.path.join(test_dir, 'fixtures/file2_nested.json')
-    result_path = os.path.join(test_dir, 'fixtures/result_plain.txt')
-
-    with open(result_path, 'r', encoding="utf-8") as f:
-        result = f.read().strip()
-
-    assert df.generate_diff(path1, path2, 'plain') == result
+    assert df.generate_diff(path1, path2, format_type) == result
